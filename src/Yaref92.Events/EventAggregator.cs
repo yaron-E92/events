12b72d44b7ac2cc443ced6f13d57edfe594a6044
Yaron Efrat <yaronefrat92@gmail.com>
2025-06-25 18:44:58 +0200

    [18]: Decouple EventAggregator from Rx
    - Removed the use of ISubject and IObservable for event publishing, replacing it with a direct call to subscribers.
    - Updated the IEventSubscriber interface to include an OnNext method for better clarity.
    - Streamlined the internal subscription management by utilizing a ConcurrentDictionary for subscribers by event type.
    - Enhanced the SubscribeToEventType and UnsubscribeFromEventType methods to reflect the new subscription model.

--- src/Yaref92.Events/EventAggregator.cs
+++ src/Yaref92.Events/EventAggregator.cs
@@ -1,10 +1,8 @@
-﻿using System.Reactive.Linq;
-using System.Reactive.Subjects;
-using System.Collections.Concurrent;
+﻿using System.Collections.Concurrent;
 using Microsoft.Extensions.Logging;
-
 using Yaref92.Events.Abstractions;
 using System.Collections.Immutable;
+using System.Linq;
 
 namespace Yaref92.Events;
 
@@ -13,22 +11,15 @@ public class EventAggregator : IEventAggregator
     private readonly ConcurrentDictionary<Type, byte> _eventTypes = new();
     public ISet<Type> EventTypes => _eventTypes.Keys.ToImmutableHashSet();
 
-    private readonly IObservable<IDomainEvent> _eventStream;
-    private readonly ISubject<IDomainEvent> _subject;
     private readonly ILogger<EventAggregator>? _logger;
-
-    private readonly ConcurrentDictionary<(Type, IEventSubscriber), List<IDisposable>> _subscriptionGroups = new();
-    public IReadOnlyCollection<IEventSubscriber> Subscribers => _subscriptionGroups.Keys.Select(k => k.Item2).ToImmutableHashSet();
-    public IReadOnlyCollection<IDisposable> Subscriptions => _subscriptionGroups.Values.SelectMany(v => v).ToImmutableHashSet();
-
+    private readonly ConcurrentDictionary<Type, ConcurrentDictionary<IEventSubscriber, byte>> _subscribersByType = new();
+    public IReadOnlyCollection<IEventSubscriber> Subscribers => _subscribersByType.Values.SelectMany(dict => dict.Keys).ToImmutableHashSet();
 
     public EventAggregator() : this(null) { }
 
     public EventAggregator(ILogger<EventAggregator>? logger)
     {
         _logger = logger;
-        _subject = Subject.Synchronize(new Subject<IDomainEvent>());
-        _eventStream = _subject.AsObservable();
     }
 
     public bool RegisterEventType<T>() where T : class, IDomainEvent
@@ -54,7 +45,13 @@ public class EventAggregator : IEventAggregator
             throw new ArgumentNullException(nameof(domainEvent), "Cannot publish a null event.");
         }
 
-        _subject.OnNext(domainEvent);
+        if (_subscribersByType.TryGetValue(typeof(T), out var subscribers))
+        {
+            foreach (var subscriber in subscribers.Keys.OfType<IEventSubscriber<T>>())
+            {
+                subscriber.OnNext(domainEvent);
+            }
+        }
     }
 
     void IEventAggregator.SubscribeToEventType<T>(IEventSubscriber<T> subscriber)
@@ -63,14 +60,8 @@ public class EventAggregator : IEventAggregator
         {
             throw new MissingEventTypeException($"The event type {nameof(T)} was not registered");
         }
-
-        var subscription = _eventStream.OfType<T>().Subscribe(subscriber);
-        var key = (typeof(T), subscriber);
-        _subscriptionGroups.AddOrUpdate(
-            key,
-            _ => new List<IDisposable> { subscription },
-            (_, list) => { lock (list) { list.Add(subscription); } return list; }
-        );
+        var dict = _subscribersByType.GetOrAdd(typeof(T), _ => new ConcurrentDictionary<IEventSubscriber, byte>());
+        dict.TryAdd(subscriber, 0);
     }
 
     void IEventAggregator.UnsubscribeFromEventType<T>(IEventSubscriber<T> subscriber)
@@ -79,17 +70,9 @@ public class EventAggregator : IEventAggregator
         {
             throw new MissingEventTypeException($"The event type {nameof(T)} was not registered");
         }
-
-        var key = (typeof(T), subscriber);
-        if (_subscriptionGroups.TryRemove(key, out var disposables))
+        if (_subscribersByType.TryGetValue(typeof(T), out var dict))
         {
-            lock (disposables)
-            {
-                foreach (var d in disposables)
-                {
-                    d.Dispose();
-                }
-            }
+            dict.TryRemove(subscriber, out _);
         }
     }
 }
