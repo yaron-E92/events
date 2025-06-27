c3d1aa788f205a00764e6985cf830222e9db32c1
Yaron Efrat <yaronefrat92@gmail.com>
2025-06-27 18:44:11 +0200

    [15]: Add AsyncRxSubscriber class for asynchronous event handling
    - Introduced the AsyncRxSubscriber class, implementing the IAsyncRxSubscriber interface for handling asynchronous events.
    - Added OnNext method for fire-and-forget async processing, along with abstract and virtual methods for async event handling and error management.
    - This addition enhances the existing reactive programming capabilities by providing a structured way to manage async event subscriptions.

---
+++ src/Yaref92.Events.Rx/AsyncRxSubscriber.cs
@@ -0,0 +1,18 @@
+﻿using Yaref92.Events.Abstractions;
+using Yaref92.Events.Rx.Abstractions;
+
+namespace Yaref92.Events.Rx;
+
+public abstract class AsyncRxSubscriber<T> : IAsyncRxSubscriber<T> where T : class, IDomainEvent
+{
+    public void OnNext(T value)
+    {
+        // Fire-and-forget async handling
+        _ = OnNextAsync(value);
+    }
+
+    public abstract Task OnNextAsync(T value);
+
+    public virtual void OnError(Exception error) { }
+    public virtual void OnCompleted() { }
+}
