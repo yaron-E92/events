f103cc7b6a5b2e8064baa9c36fcc15eef591c67c
Yaron Efrat <yaronefrat92@gmail.com>
2025-07-02 18:04:52 +0200

    [37]test: Add integration tests for TCPEventTransport
    This commit updates the solution file to include the new Yaref92.Events.IntegrationTests project and its associated files. It introduces a DummyEvent class for testing purposes and a TCPEventTransportTests class that verifies the functionality of event transmission between TCP transports. The integration test ensures that events are correctly published and received across different transport instances.

---
+++ tests/Yaref92.Events.IntegrationTests/DummyEvent.cs
@@ -0,0 +1,11 @@
+﻿using Yaref92.Events.Abstractions;
+
+namespace Yaref92.Events.IntegrationTests;
+
+/// <summary>
+/// This event is only a dummy to be used in tests
+/// </summary>
+public class DummyEvent : IDomainEvent
+{
+    public DateTime DateTimeOccurredUtc => DateTime.UtcNow;
+}
