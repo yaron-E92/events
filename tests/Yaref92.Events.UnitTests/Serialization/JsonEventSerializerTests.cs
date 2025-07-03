bb7ee41599d4c704cbc514c66cd4dfc566982a82
Yaron Efrat <yaronefrat92@gmail.com>
2025-07-03 10:35:17 +0200

    [40]test: Add unit tests for JsonEventSerializer
    This commit introduces a new test class, JsonEventSerializerTests, which includes unit tests for the JsonEventSerializer. The tests cover serialization and deserialization round-trip functionality, handling of null inputs, and validation of invalid JSON input. These tests enhance the reliability of the serialization logic and ensure proper error handling.

---
+++ tests/Yaref92.Events.UnitTests/Serialization/JsonEventSerializerTests.cs
@@ -0,0 +1,45 @@
+﻿using System.Text.Json;
+
+using FluentAssertions;
+
+using Yaref92.Events.Serialization;
+
+namespace Yaref92.Events.UnitTests.Serialization;
+
+[TestFixture]
+public class JsonEventSerializerTests
+{
+    [Test]
+    public void Serialize_And_Deserialize_RoundTrip_Works()
+    {
+        // Arrange
+        var serializer = new JsonEventSerializer();
+        var evt = new DummyEvent(DateTime.UtcNow, "test");
+
+        // Act
+        var json = serializer.Serialize(evt);
+        (Type? type, Abstractions.IDomainEvent? domainEvent) result = serializer.Deserialize(json);
+
+        // Assert
+        result.Should().NotBeNull();
+        result.Should().BeOfType<(Type? type, Abstractions.IDomainEvent? domainEvent)>();
+        result.type.Should().Be(typeof(DummyEvent));
+        result.domainEvent.Should().NotBeNull().And.BeEquivalentTo(evt);
+    }
+
+    [Test]
+    public void Serialize_Null_Throws()
+    {
+        var serializer = new JsonEventSerializer();
+        Action act = () => serializer.Serialize<DummyEvent>(null!);
+        act.Should().Throw<ArgumentNullException>();
+    }
+
+    [Test]
+    public void Deserialize_InvalidJson_Throws()
+    {
+        var serializer = new JsonEventSerializer();
+        Action act = () => serializer.Deserialize("not a json");
+        act.Should().Throw<JsonException>();
+    }
+} 
