df462af51d77ec319d1b202cfb519897c01ccf93
Yaron Efrat <yaronefrat92@gmail.com>
2025-11-21 21:35:43 +0100

    Handle duplicate Rx subscriptions

--- tests/Yaref92.Events.Rx.UnitTests/RxEventAggregatorTests.cs
+++ tests/Yaref92.Events.Rx.UnitTests/RxEventAggregatorTests.cs
@@ -89,6 +89,26 @@ public class RxEventAggregatorTests
     }
 
     [Test]
+    public void SubscribeToEventType_DuplicateRxSubscriber_DoesNotKeepUntrackedSubscription()
+    {
+        // Arrange
+        var rxSubscriber = new RxDummySubscriber();
+        _aggregator.SubscribeToEventType(rxSubscriber);
+
+        // Act
+        _aggregator.SubscribeToEventType(rxSubscriber);
+        var evt = new DummyEvent();
+        _aggregator.PublishEvent(evt);
+
+        _aggregator.UnsubscribeFromEventType(rxSubscriber);
+        _aggregator.PublishEvent(new DummyEvent());
+
+        // Assert
+        rxSubscriber.ReceivedEvent.Should().Be(evt);
+        rxSubscriber.OnNextCount.Should().Be(1);
+    }
+
+    [Test]
     public void PublishEvent_MixedRegularAndRxSubscribers_BothReceiveEvent()
     {
         // Arrange
